860547ee32c62d56cedc9761264ddcf123e18f4d
Microler <414464322@qq.com>
2022-11-07 23:53:47 +0800

    feat: preview build

--- packages/neumorphism-preview/vite.config.ts
+++ packages/neumorphism-preview/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    // terserOptions: {
+    //   compress: {
+    //     keep_infinity: true,
+    //     drop_console: true
+    //   }
+    // },
+    // minify: 'terser',
+    // chunk 大小警告的限制
+    chunkSizeWarningLimit: 2000,
+    outDir: 'dist'
+  },
 })
