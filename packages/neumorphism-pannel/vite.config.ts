5120ae5be47861e3739244086be7c8d2b9d6b3bf
Microler <414464322@qq.com>
2022-11-09 07:46:03 +0800

    feat: working on vite-rollup config in pannel package

--- packages/neumorphism-pannel/vite.config.ts
+++ packages/neumorphism-pannel/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.ts',
+      external: ['react'],
+      output: {
+        globals: { react: 'react' },
+        dir: 'dist',
+        format: 'commonjs'
+      }
+    }
+  }
 })
