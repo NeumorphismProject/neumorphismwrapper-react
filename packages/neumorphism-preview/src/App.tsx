29f9b7fdad1401270a791692aec372f565a261f9
Microler <414464322@qq.com>
2022-11-12 09:47:53 +0800

    feat: add mui and tailwindcss

--- packages/neumorphism-preview/src/App.tsx
+++ packages/neumorphism-preview/src/App.tsx
@@ -1,49 +1,29 @@
 import { useState, useCallback } from 'react'
 import { NeumorphismPannel, getNeumorphismStyle, isValidColor } from 'neumorphism-pannel'
+import Sidebar from './components/Sidebar'
 
 function App() {
   const [color, setColor] = useState<string>('#83abfb');
-  const colorOnChange = ({ target: { value } }: any) => {
-    if (isValidColor(value)) {
-      setColor(value)
+  const handleColorChange = (colorHex: string) => {
+    if (isValidColor(colorHex)) {
+      setColor(colorHex)
     }
   }
-  const handleColor = useCallback((e: any) => {
-    setColor(e.target.value)
-  }, [])
   const neumorphismStyle = getNeumorphismStyle({ color });
   const styleForReactString = JSON.stringify(neumorphismStyle, null, '\t')
   return (
-    <div className="w-screen h-screen" style={{ backgroundColor: color }}>
-      <div className="row">
-        <label htmlFor="color">Pick a color:</label>
-        <input
-          type="color"
-          name="color"
-          onChange={handleColor}
-          placeholder="#ffffff"
-          value={color}
-          id="color"
-        />
-        <label htmlFor="colorInput" style={{ paddingLeft: '10px' }}>
-          or
-        </label>
-        <input
-          type="text"
-          placeholder="#ffffff"
-          name="color"
-          onChange={colorOnChange}
-          value={color}
-        />
+    <div className="w-screen h-screen flex" style={{ backgroundColor: color }}>
+      <div className="w-64 h-full">
+        <Sidebar color={color} onColorChange={handleColorChange} styleForReactString={styleForReactString} />
       </div>
-      <NeumorphismPannel color={color} style={{
-        width: '300px',
-        height: '300px',
-        margin: '0 auto'
-      }}>
-      </NeumorphismPannel>
-      <div className='code-view-label'>react style</div>
-      <textarea className='code-view'>{styleForReactString}</textarea>
+      <div className="flex-1">
+        <NeumorphismPannel color={color} style={{
+          width: '300px',
+          height: '300px',
+          margin: '0 auto'
+        }}>
+        </NeumorphismPannel>
+      </div>
     </div>
   )
 }
--- packages/neumorphism-preview/src/components/Sidebar/index.tsx
+++ packages/neumorphism-preview/src/components/Sidebar/index.tsx
@@ -1,5 +1,40 @@
 import React from "react";
 
-export default function Sidebar() {
-  return <div></div>
+export interface SidebarProps {
+  color: string
+  onColorChange: (colorHex: string) => void
+  styleForReactString: string
+}
+export default function Sidebar({ color, onColorChange, styleForReactString }: SidebarProps) {
+  const handleColorChange = (e: any) => {
+    onColorChange(e.target.value)
+  }
+  return <div className="w-full h-full flex flex-col">
+    <div className="h-32">
+      <label htmlFor="color">Pick a color:</label>
+      <input
+        type="color"
+        name="color"
+        onChange={handleColorChange}
+        placeholder="#ffffff"
+        value={color}
+        id="color"
+      />
+      <label htmlFor="colorInput" style={{ paddingLeft: '10px' }}>
+        or
+      </label>
+      <input
+        type="text"
+        placeholder="#ffffff"
+        name="color"
+        onChange={handleColorChange}
+        value={color}
+      />
+    </div>
+    {/* ------------ */}
+    <div>
+      <div className=''>react style</div>
+      <textarea className='w-96 h-64'>{styleForReactString}</textarea>
+    </div>
+  </div>
 }
